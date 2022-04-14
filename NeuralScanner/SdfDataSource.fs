7235ae1b68d096fdf3cba38541c349aac9dab478
Frank A. Krueger <fak@praeclarum.org>
2022-04-14 13:50:43 -0700

    Only train on visible scans

--- NeuralScanner/SdfDataSource.fs
+++ NeuralScanner/SdfDataSource.fs
@@ -132,7 +132,7 @@ type SdfDataSet (project : Project, samplingDistance : float32, outputScale : fl
     override this.GetRow (index, _) =
         let inside = (index % 2) = 0
         let mutable fi = StaticRandom.Next(frames.Length)
-        while not (frames.[fi].HasRows) do
+        while not (frames.[fi].Visible && frames.[fi].HasRows) do
             fi <- StaticRandom.Next(frames.Length)
         let struct (i, o) = frames.[fi].GetRow (inside, volumeCenter, samplingDistance, outputScale, unoccupied, occupancy.NumCells, batchData)
         //printfn "ROW%A D%A = %A" index inside i.[2].[0]
