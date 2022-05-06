72933857eedb80f1fd3070de8cb290fd98e10f39
Frank A. Krueger <fak@praeclarum.org>
2022-05-06 13:28:37 -0700

    Output SDFs

--- ObjectToSdf/AppDelegate.fs
+++ ObjectToSdf/AppDelegate.fs
@@ -8,46 +8,63 @@ open g3
 type O2S () =
 
     let inputDirs = [|"/Volumes/nn/Data/GoogleScannedObjects" |]
+    let outputDir = "/Volumes/nn/Data/datasets/sdfs"
+
+    do if Directory.Exists outputDir |> not then Directory.CreateDirectory outputDir |> ignore
+
     let files =
         inputDirs
         |> Array.collect (fun d ->
             Directory.GetFiles(d, "*.zip", SearchOption.AllDirectories))
 
-    let numCells = 128
+    let numCells = 512
     let cellSize = 2.0 / float numCells
-    let numPoints = 10
+    let numPoints = 200_000
 
     let processObjBytes (name : string) (stream : Stream) =
         let md5 = Security.Cryptography.MD5.Create()
         let hash = md5.ComputeHash(stream)
         let hashSum = Seq.sum (hash |> Seq.map int)
-        stream.Position <- 0L
-
-        let random = Random(hashSum)
-        let mesh = StandardMeshReader.ReadMesh(stream, "obj")
-        let bounds = mesh.GetBounds ()
-        MeshTransforms.ConvertZUpToYUp (mesh)
-        let ybounds = mesh.GetBounds ()
-        MeshTransforms.Translate (mesh, -ybounds.Center)
-        let cbounds = mesh.GetBounds ()
-        let scale = 0.9 + 0.1 * random.NextDouble ()
-        MeshTransforms.Scale (mesh, scale/cbounds.Extents.x, scale/cbounds.Extents.y, scale/cbounds.Extents.z)
-        let sbounds = mesh.GetBounds ()
-        let sdf = new MeshSignedDistanceGrid(mesh, cellSize)
-        sdf.Compute()
-        let iso = DenseGridTrilinearImplicit(sdf.Grid, Vector3d(sdf.GridOrigin), cellSize)
-        for i in 0..(numPoints - 1) do
-            let mutable p = Vector3d (random.NextDouble () * 2.0 - 1.0, random.NextDouble () * 2.0 - 1.0, random.NextDouble () * 2.0 - 1.0)
-            let d = iso.Value (&p)
+        let hashString = String.Join ("", hash |> Seq.map (fun x -> x.ToString("x2")))
+        let outputPath = Path.Combine (outputDir, (hashString + ".sdf"))
+        if File.Exists outputPath then
             ()
-        ()
+        else
+            stream.Position <- 0L
+
+            let random = Random(hashSum)
+            let mesh = StandardMeshReader.ReadMesh(stream, "obj")
+            let bounds = mesh.GetBounds ()
+            MeshTransforms.ConvertZUpToYUp (mesh)
+            let ybounds = mesh.GetBounds ()
+            MeshTransforms.Translate (mesh, -ybounds.Center)
+            let cbounds = mesh.GetBounds ()
+            let scale = 0.9 + 0.1 * random.NextDouble ()
+            MeshTransforms.Scale (mesh, scale/cbounds.Extents.x, scale/cbounds.Extents.y, scale/cbounds.Extents.z)
+            let sbounds = mesh.GetBounds ()
+            let sdf = new MeshSignedDistanceGrid(mesh, cellSize)
+            sdf.UseParallel <- true
+            sdf.Compute()
+            let iso = DenseGridTrilinearImplicit(sdf.Grid, Vector3d(sdf.GridOrigin), cellSize)
+            let tempPath =
+                let p = Path.GetTempFileName ()
+                use s = new FileStream(p, FileMode.Create, FileAccess.Write)
+                use w = new BinaryWriter (s)
+                for i in 0..(numPoints - 1) do
+                    let mutable p = Vector3d (random.NextDouble () * 2.0 - 1.0, random.NextDouble () * 2.0 - 1.0, random.NextDouble () * 2.0 - 1.0)
+                    let d = iso.Value (&p)
+                    w.Write(float32 p.x)
+                    w.Write(float32 p.y)
+                    w.Write(float32 p.z)
+                    w.Write(d)
+                p
+            File.Move (tempPath, outputPath)
 
     let mutable numFilesProc = 0
 
     let processFile (zipPath : string) =
         //printfn "ZIP: %s" zipPath
-        let n = Threading.Interlocked.Increment(&numFilesProc)
-        let name = (Path.GetFileName(zipPath))
+        let name = (Path.GetFileNameWithoutExtension(zipPath))
         use zs = File.OpenRead zipPath
         use z = new Compression.ZipArchive (zs)
         z.Entries
@@ -56,18 +73,20 @@ type O2S () =
             use s = x.Open()
             use d = new MemoryStream(int x.Length)
             s.CopyTo d
-            let progress = 100.0 * (float numFilesProc / float files.Length)
             d.Position <- 0L
-            processObjBytes name d
+            processObjBytes (sprintf "%s_%d" name i) d
+            let progress = 100.0 * (float numFilesProc / float files.Length)
             printfn "[%.1f] %s/%s (%d)" progress name x.FullName d.Length
             ())
+        let _ = Threading.Interlocked.Increment(&numFilesProc)
         ()
 
     member this.Files = files
 
     member this.Run () =
         printfn "%d FILES" this.Files.Length
-        Threading.Tasks.Parallel.ForEach (files, processFile) |> ignore
+        let options = Threading.Tasks.ParallelOptions(MaxDegreeOfParallelism = 3)
+        Threading.Tasks.Parallel.ForEach (files, options, processFile) |> ignore
         ()
 
 
